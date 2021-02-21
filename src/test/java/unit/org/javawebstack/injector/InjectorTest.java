32934da133839b9dc005893b1f835d2bd9405f6d
x7airworker <jake@jakes-co.de>
2021-02-21 16:33:08 +0100

    Added method make and constructor injection

--- src/test/java/unit/org/javawebstack/injector/InjectorTest.java
+++ src/test/java/unit/org/javawebstack/injector/InjectorTest.java
@@ -31,6 +31,23 @@ public class InjectorTest {
         assertEquals("Test", test.getB());
     }
 
+    @Test
+    public void testConstructorInjection() {
+        SimpleInjector injector = new SimpleInjector();
+        injector.setInstance(String.class, "Test");
+        injector.setInstance(String.class, "named", "NamedTest");
+        injector.setInstance(Integer.class, 1337);
+
+        Test3 test = injector.make(Test3.class);
+
+        assertEquals(test.getTest(), "Test");
+        assertEquals(test.getNamedTest(), "NamedTest");
+        assertEquals(test.getFieldInjection(), 1337);
+
+        Test4 zArgsConstructorTest = injector.make(Test4.class);
+        assertEquals(zArgsConstructorTest.getTest(), "Test");
+    }
+
     private static class Test1 extends Test2 {
         @Inject
         private String a;
@@ -47,4 +64,37 @@ public class InjectorTest {
         }
     }
 
+    private static class Test3 {
+        private String test;
+        private String namedTest;
+        @Inject
+        private Integer fieldInjection;
+
+        @Inject
+        public Test3(String test, @Inject("named") String namedTest) {
+            this.test = test;
+            this.namedTest = namedTest;
+        }
+
+        public String getTest() {
+            return test;
+        }
+
+        public String getNamedTest() {
+            return namedTest;
+        }
+
+        public Integer getFieldInjection() {
+            return fieldInjection;
+        }
+    }
+
+    private static class Test4 {
+        @Inject
+        private String test;
+
+        public String getTest() {
+            return test;
+        }
+    }
 }
